d746fcd5447e57bebf37c0824d65dc234d1c095c
Alasdair Monk <alasdair.monk@gmail.com>
2016-08-31 15:11:04 +0100

    Initial Commit

---
+++ Keeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Keeper
+//
+//  Created by Alasdair Monk on 31/08/2016.
+//  Copyright © 2016 Alasdair Monk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
